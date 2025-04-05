c6e8f4f2758e19470be8b72540091c375bb45e37
DinhLongHuynh <Dinh-Long.Huynh.4289@student.uu.se>
2025-04-05 13:24:45 +0200

    updata smiles_generator

--- scripts/fdadrug_fetchting.py
+++ scripts/fdadrug_fetchting.py
@@ -1,7 +1,7 @@
 import os 
 import sys
 sys.path.append(os.path.abspath('../'))
-from utils.data_processing import fda_downloader, unziper, csv_converter
+from utils.data_processing import fda_downloader, unziper, csv_converter, smile_generator
 
 
 # Run:
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     fda_downloader()
     unziper(file_path='../data_raw/fda_data_raw.zip',export_path='../data_raw')
     csv_converter(file_path='../data_raw',export_path='../data_csv')
+    smile_generator(data_path='../data_csv/Products.csv')
